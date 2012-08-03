c045c562e13e1fdc263f1b1dfdb0a37549ba62b6
asdlvs <asdfghjklzxc@yandex.ru>
2012-08-03 19:40:42 +0400

    dropdown

---
+++ MinStat.AnalizeUI/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinStat.AnalizeUI.Controllers
+{
+  using MinStat.AnalizeUI.Models;
+  using MinStat.AnalizeUI.ServiceAdapters;
+
+  public class CommonController : Controller
+  {
+    private readonly ConsolidateReportAdapter _adapter;
+    private readonly IInfoDataAdapter _infoAdapter;
+
+    public CommonController()
+    {
+      _adapter = new ConsolidateReportAdapter();
+      _infoAdapter = new InfoDataAdapter();
+    }
+
+    public ActionResult Report(int enterpriseId, int federalSubjectId, int federalDistrictId, int activityId)
+    {
+      IEnumerable<StatisticDataModel> statisticData = _adapter.GetDynamicConsolidatedReport(null);
+      return View("DynamicStatisticData", statisticData);
+    }
+
+  }
+}
--- MinStat.DAL/Converters/SummaryDataReportConverter.cs
+++ MinStat.DAL/Converters/SummaryDataReportConverter.cs
@@ -13,57 +13,61 @@ namespace MinStat.DAL.Converters
         public IEnumerable<StatisticData> Convert(IEnumerable<SummaryReportItem> result)
         {
             StatisticData statisticData = new StatisticData();
-
-            statisticData.Titles = new Dictionary<string, string>
-                {
-                    {"0", ""},
-                    {"1", "Количество"},
-                    {"2", "Средний возраст"},
-                    {"3", "Средний годовой доход"}
-                };
-            statisticData.Lines = new List<StatisticDataItem>();
             var resultList = result.ToList();
-
-            var groupedByActivityResult = resultList.GroupBy(x => new { x.ActivityId, x.ActivityTitle });
-            foreach (var groupedItem in groupedByActivityResult)
+            if (resultList.Any())
             {
+              statisticData.Titles = new Dictionary<string, string>
+                { { "0", "" }, { "1", "Количество" }, { "2", "Средний возраст" }, { "3", "Средний годовой доход" } };
+              statisticData.Lines = new List<StatisticDataItem>();
+
+              var groupedByActivityResult = resultList.GroupBy(x => new { x.ActivityId, x.ActivityTitle });
+              foreach (var groupedItem in groupedByActivityResult)
+              {
                 StatisticDataItem dataItem = new StatisticDataItem();
                 dataItem.StrongLevel = 1;
                 dataItem.Title = groupedItem.Key.ActivityTitle;
                 dataItem.Values = new List<string>
-                                      {
-                                          groupedItem.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
-                                          groupedItem.Average(x => x.MiddleAge).ToString("0.00"),
-                                          groupedItem.Average(x => x.MiddleSalary).ToString("0.00")
-                                      };
+                  {
+                    groupedItem.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
+                    groupedItem.Average(x => x.MiddleAge).ToString("0.00"),
+                    groupedItem.Average(x => x.MiddleSalary).ToString("0.00")
+                  };
                 statisticData.Lines.Add(dataItem);
-                var groupedByGPEItem = groupedItem.GroupBy(x => String.Format("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Пол: {0}, Категория: {1}, Образование: {2}", x.Gender ? "М" : "Ж",
-                                  x.PostLevelTitle, x.EducationLevelTitle));
+                var groupedByGPEItem =
+                  groupedItem.GroupBy(
+                    x =>
+                    String.Format(
+                      "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Пол: {0}, Категория: {1}, Образование: {2}",
+                      x.Gender ? "М" : "Ж",
+                      x.PostLevelTitle,
+                      x.EducationLevelTitle));
 
                 foreach (var gpeGroupedItem in groupedByGPEItem)
                 {
-                    StatisticDataItem innerDataItem = new StatisticDataItem();
-                    innerDataItem.Title = gpeGroupedItem.Key;
-                    innerDataItem.Values = new List<string>
-                                               {
-                                                   gpeGroupedItem.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
-                                                   gpeGroupedItem.Average(x => x.MiddleAge).ToString("0.00"),
-                                                   gpeGroupedItem.Average(x => x.MiddleSalary).ToString("0.00")
-                                               };
-                    statisticData.Lines.Add(innerDataItem);
+                  StatisticDataItem innerDataItem = new StatisticDataItem();
+                  innerDataItem.Title = gpeGroupedItem.Key;
+                  innerDataItem.Values = new List<string>
+                    {
+                      gpeGroupedItem.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
+                      gpeGroupedItem.Average(x => x.MiddleAge).ToString("0.00"),
+                      gpeGroupedItem.Average(x => x.MiddleSalary).ToString("0.00")
+                    };
+                  statisticData.Lines.Add(innerDataItem);
                 }
-            }
-            StatisticDataItem globalDataItem = new StatisticDataItem();
-            globalDataItem.Title = "Итого: ";
-            globalDataItem.StrongLevel = 2;
-            globalDataItem.Values = new List<string>{
-                                                   resultList.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
-                                                   resultList.Average(x => x.MiddleAge).ToString("0.00"),
-                                                   resultList.Average(x => x.MiddleSalary).ToString("0.00")
-                                               };
+              }
+              StatisticDataItem globalDataItem = new StatisticDataItem();
+              globalDataItem.Title = "Итого: ";
+              globalDataItem.StrongLevel = 2;
+              globalDataItem.Values = new List<string>
+                {
+                  resultList.Sum(x => x.Count).ToString(CultureInfo.InvariantCulture),
+                  resultList.Average(x => x.MiddleAge).ToString("0.00"),
+                  resultList.Average(x => x.MiddleSalary).ToString("0.00")
+                };
 
-            statisticData.Lines.Add(globalDataItem);
-            return new List<StatisticData> { statisticData };
+              statisticData.Lines.Add(globalDataItem);
+            }
+          return new List<StatisticData> { statisticData };
         }
 
         public IEnumerable<POCO.ResultItems.StatisticData> Convert(IEnumerable<SummaryReportItem> result, List<int> criteries)
